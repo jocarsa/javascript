51083ddef0b0903d973beeb33002456c9116af63
jocarsa <info@josevicentecarratala.com>
2023-06-05 14:11:55 +0200

    Actualizar 303-codigo.acomment.js

---
+++ 300-Fundamentos Javascript/001-Sintaxis/303-codigo.acomment.js
@@ -0,0 +1,7 @@
+ A continuación, vamos a suponer que creamos un archivo de código fuera del archivo HTML, por ejemplo, justo a su lado.
+
+Dentro de este código, por el momento introduciremos una instrucción sencilla que, en los siguientes apartados, dentro de este mismo capítulo, explicaremos, como es la salida por consola.
+
+Esta instrucción saca una cadena de texto por consola, donde en este caso utilizaremos la cadena hola mundo.
+
+Es importante por lo tanto tener en cuenta que estamos creando esta línea de código, en un archivo JavaScript, totalmente independiente del archivo HTML, que vamos a crear a continuación. 
